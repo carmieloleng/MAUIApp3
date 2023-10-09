b0b07b096c997c616f912defd974f31aadc24bf8
carmieloleng <126441713+carmieloleng@users.noreply.github.com>
2023-10-09 08:42:30 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MAUIApp3;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	async void OnDial(object sender, EventArgs e)
+	{
+		try
+		{
+			PhoneDialer.Open(PhoneNum.Text);
+			Convert.ToInt32(PhoneNum.Text);
+		}
+		catch
+		{
+			await DisplayAlert("Invalid input", "Please enter a valid phone number", "OK");
+        }
+
+	}
+}
